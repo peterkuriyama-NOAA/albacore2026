777a8dddb32737df22f4c3ee795bf5610b83949e
peterkuriyama-NOAA <peter.kuriyama@noaa.gov>
2026-03-30 17:56:50 +0000

    script to copy files to bucket

---
+++ R/move_to_bucket.R
@@ -0,0 +1,72 @@
+#------------------------------------------------------------------------
+#Backup model results to data bucket
+
+
+#------------------------------------------------------------------------
+#Mount bucket to cloud work station
+
+#---System commands 
+#See these instructions to use gcsfuse to mount bucket to workstation
+# file:///C:/Users/peter.kuriyama/Downloads/SWFSC_MSECloud_Helper%20(1).html
+
+
+# system("sudo apt-get update")
+# system("sudo apt-get install -y curl lsb-release")
+# 
+# system("export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`")
+# system('echo "deb [signed-by=/usr/share/keyrings/cloud.google.asc] https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list')
+# 
+# 
+# 
+# system("sudo apt-get update")
+# system("sudo apt-get install gcsfuse")
+# 
+# 
+# system("gcsfuse frd_popdy_peter '$HOME/bucket'")
+
+#------------------------------------------------------------------------
+#Start of file copying
+library(foreach)
+library(doParallel)
+
+#Specify directories to add
+bucketdir <- "/home/user/bucket/albacore/model"
+workstationdir <- "/home/user/2026_albacore/model"
+  
+bucket_files <- list.files(bucketdir)
+workstation_files <- list.files(workstationdir)
+
+#Files not yet uploaded
+files_to_upload <- workstation_files[which(workstation_files %in% bucket_files == FALSE)]
+#Keep base_model_2026 on workstation
+files_to_upload <- files_to_upload[which(files_to_upload != "base_model_2026")]
+#Also only keep folders that have mod or day in the name
+files_to_upload <- files_to_upload[grep("day|mod", files_to_upload)]
+
+files_to_upload <- files_to_upload[-grep("\\.ss|\\.par", files_to_upload)]
+
+
+
+# files_to_upload_dirs <- paste0(bucketdir, "/", files_to_upload)
+
+#Upload the files in parallel
+# ncores <- 10
+# 
+# 
+# cl <- makeCluster(ncores)
+# registerDoParallel(cl)
+
+ii <- 1
+
+
+fromdir <- paste0(workstationdir, "/",files_to_upload[ii])
+todir <- paste0(bucketdir, "/", files_to_upload[ii])
+
+dir.create(todir, recursive = T)
+
+mv_command <- paste0("mv ", fromdir, "* ", todir, '')
+system(mv_command)
+  
+
+
+
