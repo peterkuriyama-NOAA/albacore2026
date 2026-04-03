1aefaa7d924326545bfebd2b4f0584cc9ca079c5
peterkuriyama-NOAA <peter.kuriyama@noaa.gov>
2026-04-03 18:27:45 +0000

    updated sensitivity checks

--- R/sensitivities_v2.R
+++ R/sensitivities_v2.R
@@ -17,9 +17,10 @@ library(r4ss)
 # devtools::install_github("peterkuriyama/cpsassessment")
 library(cpsassessment)
 
-# detach("package:hmsassessment", unload = TRUE)
+detach("package:hmsassessment", unload = TRUE)
 devtools::install_github("peterkuriyama-NOAA/hmsassessment")
 library(hmsassessment)
+# devtools::load_all("/home/user/hmsassessment/")
 
 #  "../../../ss3.30.24_linux/ss3"
 #-------------------------------------------------------------------------------
@@ -28,30 +29,132 @@ library(hmsassessment)
 
 #Steepness--------------------------------------
 #h = 0.75, 0.8, 0.85
+
+###h=.75----
 change_parvalue(fromdir = "model/base_model_2026/", todir = "model/sensitivities/steep75/",
                 run_from_par = T, par_to_change = "steep",
                 parval_new = data.frame(INIT = c(.75), PHASE = c(-1)))
 
+#Tune recdevs
+change_parvalue(fromdir = "model/sensitivities/steep75/", 
+                todir = "model/sensitivities/steep75_tuned/",
+                run_from_par = T, par_to_change = "steep",
+                parval_new = data.frame(INIT = c(.75), PHASE = c(-1)))
+
+newctl <- do_biasadj("model/sensitivities/steep75/", ctlname = "control.ss_new")
+SS_writectl(ctllist = newctl, outfile = paste0("model/sensitivities/steep75_tuned/", 
+                                               "control.ss"), overwrite = T)
+
+###h=.80----
 change_parvalue(fromdir = "model/base_model_2026/", todir = "model/sensitivities/steep80/",
                 run_from_par = T, par_to_change = "steep",
                 parval_new = data.frame(INIT = c(.80), PHASE = c(-1)))
 
+#Tune recdevs
+change_parvalue(fromdir = "model/sensitivities/steep80/", 
+                todir = "model/sensitivities/steep80_tuned/",
+                run_from_par = T, par_to_change = "steep",
+                parval_new = data.frame(INIT = c(.80), PHASE = c(-1)))
 
+newctl <- do_biasadj("model/sensitivities/steep80/", ctlname = "control.ss_new")
+SS_writectl(ctllist = newctl, outfile = paste0("model/sensitivities/steep80_tuned/", 
+                                               "control.ss"), overwrite = T)
+
+
+###h=.85----
 change_parvalue(fromdir = "model/base_model_2026/", todir = "model/sensitivities/steep85/",
                 run_from_par = T, par_to_change = "steep",
                 parval_new = data.frame(INIT = c(.85), PHASE = c(-1)))
 
+change_parvalue(fromdir = "model/sensitivities/steep85/", 
+                todir = "model/sensitivities/steep85_tuned/",
+                run_from_par = T, par_to_change = "steep",
+                parval_new = data.frame(INIT = c(.85), PHASE = c(-1)))
+newctl <- do_biasadj("model/sensitivities/steep85/", ctlname = "control.ss_new")
+SS_writectl(ctllist = newctl, outfile = paste0("model/sensitivities/steep85_tuned/", 
+                                               "control.ss"), overwrite = T)
+
+
+###Steep prior----
+#TODO
+change_parvalue(fromdir = "model/base_model_2026/", todir = "model/sensitivities/steep_est/",
+                run_from_par = F, 
+                par_to_change = "steep",
+                parval_new = data.frame(INIT = c(.9), PRIOR = c(.9), 
+                                        PR_SD = c(0.1), PR_type = c(6),PHASE = c(4)))
+
+#Tune recdevs
+change_parvalue(fromdir = "model/sensitivities/steep_est/", 
+                todir = "model/sensitivities/steep_est_tuned/",
+                run_from_par = T, 
+                par_to_change = "steep",
+                parval_new = data.frame(INIT = c(.9), PRIOR = c(.9), 
+                                        PR_SD = c(0.1), PR_type = c(6),PHASE = c(4)))
+newctl <- do_biasadj("model/sensitivities/steep_est/", ctlname = "control.ss_new")
+SS_writectl(ctllist = newctl, outfile = paste0("model/sensitivities/steep_est_tuned/", 
+                                               "control.ss"), overwrite = T)
+
+
+
+#-----
+#Read in results
+
 #Growth--------------------------------------
 ##CV Old
-#CV = .06
+#CV = .06---------------
 change_parvalue(fromdir = "model/base_model_2026/", todir = "model/sensitivities/CVold6/",
                 run_from_par = T, par_to_change = "CV_old_Fem",
                 parval_new = data.frame(INIT = c(.06), PHASE = c(-1)))
 
-#CV = .08
+#Tune recdevs
+change_parvalue(fromdir = "model/sensitivities/CVold6/", 
+                todir = "model/sensitivities/CVold6_tuned/",
+                run_from_par = T, 
+                par_to_change = "CV_old_Fem",
+                parval_new = data.frame(INIT = c(.06), PHASE = c(-1)))
+
+
+newctl <- do_biasadj("model/sensitivities/CVold6/", ctlname = "control.ss_new")
+SS_writectl(ctllist = newctl, outfile = paste0("model/sensitivities/CVold6_tuned/", 
+                                               "control.ss"), overwrite = T)
+
+
+
+
+#CV = .08----------------
 change_parvalue(fromdir = "model/base_model_2026/", todir = "model/sensitivities/CVold8/",
                 run_from_par = T, par_to_change = "CV_old_Fem",
                 parval_new = data.frame(INIT = c(.08), PHASE = c(-1)))
+
+change_parvalue(fromdir = "model/sensitivities/CVold8/", 
+                todir = "model/sensitivities/CVold8_tuned/",
+                run_from_par = T, par_to_change = "CV_old_Fem",
+                parval_new = data.frame(INIT = c(.08), PHASE = c(-1)))
+newctl <- do_biasadj("model/sensitivities/CVold8/", ctlname = "control.ss_new")
+SS_writectl(ctllist = newctl, outfile = paste0("model/sensitivities/CVold8_tuned/", 
+                                               "control.ss"), overwrite = T)
+
+
+
+
+
+
+
+
+cvres <- ssoutput_parallel(ncores = 5, folders = c("model/base_model_2026",
+                                                      "model/sensitivities/CVold6/",
+                                                      "model/sensitivities/CVold8/"))
+
+names(cvres) <- c("base", "CV.06", "CV.08")
+cvsumm <- SSsummarize(cvres)
+cvsumm$likelihoods
+cvsumm$maxgrad
+cvsumm$pars %>% slice(grep("R0", Label))
+
+SSplotComparisons(cvsumm, subplot = 2)
+
+
+
 
 #InitF--------------------------------------
 #Currently initF is only on F26
@@ -94,3 +197,19 @@ change_initF(basedir = "model/base_model_2026",
 change_initF(basedir = "model/base_model_2026",
              newdir = "model/sensitivities/initF_F1_F20/",
              initF_fleets = c(1, 20))
+
+initFres <- ssoutput_parallel(ncores = 5, folders = c("model/base_model_2026",
+                    "model/sensitivities/initF_F26_F34/",
+                    "model/sensitivities/initF_F20_F26/",
+                    "model/sensitivities/initF_F20_F24/",
+                    "model/sensitivities/initF_F1_F20/"))
+
+names(initFres) <- c("base_F26", "F26_F34", "F20_F26", "F20_F24", "F1_F20")
+initFsumm <- SSsummarize(initFres)
+initFsumm$likelihoods
+initFsumm$maxgrad
+initFsumm$pars %>% slice(grep("R0", Label))
+
+SSplotComparisons(initFsumm, subplot = 2)
+
+
